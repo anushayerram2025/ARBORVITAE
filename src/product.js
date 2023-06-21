21c6d5ef81320f8457ca5c6f1249196b4d8a2c42
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2023-06-21 23:02:31 +0300

    Update product.js

--- src/product.js
+++ src/product.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, Suspense } from 'react';
-import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './index.css';
